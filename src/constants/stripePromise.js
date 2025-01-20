79711d05976557a653383269cf45cff3ebc87537
Anup_developer <anup42564@gmail.com>
2025-01-20 23:25:12 +0530

    jan 20 commit

--- src/constants/stripePromise.js
+++ src/constants/stripePromise.js
@@ -1,3 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe("pk_live_51QgJMCJ5DtaLPeLIkChdFbdwUER6NaAEQHhOI0dpvYbyzVPAIyIaUUGljQ5lYOr3QHEKsggeUUXaucvLIggjSAlX00Xh836u2n");
+export const stripePromise = loadStripe("pk_live_51QgJMCJ5DtaLPeLIx4N2b0pYgbPnv2TElyHxjnnCBtMdW5MQtnKVZ3FA7YjeI6cIHGUeHKqTApjSVlh5bmxLNocu00seuW8H1Y");
